51005455c1ec5bc0e54d569f1e5b69107b923e4a
Jacob Scobie <jscobie@focus-solutions.net>
2022-10-05 18:18:37 -0400

    preclass work

--- assets/script.js
+++ assets/script.js
@@ -13,17 +13,26 @@ const correctAnswers = ["alerts", "parenthesis", "all of the above", "quotes", "
 var startButton = document.querySelector("#start-button");
 var startScreen = document.querySelector("#start-screen")
 var timer = document.querySelector("#countdown");
-var questionText = document.querySelector("#questions")
-var answerButtons = document.querySelector("#answers")
+var questionText = document.querySelector("#questions");
+var answerButtons = document.querySelector("#answers");
+var button1 = document.querySelector("#choice1");
+var button2 = document.querySelector("#choice2");
+var button3 = document.querySelector("#choice3");
+var button4 = document.querySelector("#choice4");
 
 var secondsLeft = timer.textContent;
 
 startButton.addEventListener("click", function(event) {
     startTimer();
     startScreen.setAttribute("hidden", "true");
+    startScreen.setAttribute("display", "");
     questionText.removeAttribute("hidden", "true");
+    questionText.textContent = questions[0];
+    button1.textContent = oneChoice[0];
+    button2.textContent = oneChoice[1];
+    button3.textContent = oneChoice[2];
+    button4.textContent = oneChoice[3];
     answerButtons.removeAttribute("hidden", "true");
-    answerButtons.setAttribute("display", "table-column")
 });
 
 function startTimer() {
