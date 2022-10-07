d35d00efe1555c85cc6a497716b4c04611167a3c
Jacob Scobie <jscobie@focus-solutions.net>
2022-10-06 21:43:22 -0400

    launching ideas after class

--- assets/script.js
+++ assets/script.js
@@ -1,13 +1,28 @@
-const questions = ["Commonly used data types DO NOT include:", 
-"The condition in an if / else statement is enclosed within _______.", 
-"Arrays in JavaScript can be used to store _______.",
-"String values must be enclosed within ________ when being assigned to variables",
-"A very useful tool used during development and debugging for printing content to the debugger is:"];
-const oneChoice = ["strings", "booleans", "alerts","numbers"];
-const twoChoice = ["quotes", "curly brackets", "parenthesis", "square brackets"];
-const threeChoice = ["numbers and strings", "other arrays", "booleans", "all of the above"];
-const fourChoice = ["commas", "curly brackets", "quotes", "parenthesis"];
-const fiveChoice = ["JavaScript", "terminal/bash", "for loops", "console.log"];
+const oneChoice = ["Commonly used data types DO NOT include:",
+"strings", 
+"booleans", 
+"alerts",
+"numbers"];
+const twoChoice = ["The condition in an if / else statement is enclosed within _______.",
+"quotes", 
+"curly brackets", 
+"parenthesis", 
+"square brackets"];
+const threeChoice = ["Arrays in JavaScript can be used to store _______.",
+"numbers and strings", 
+"other arrays", 
+"booleans", 
+"all of the above"];
+const fourChoice = ["String values must be enclosed within ________ when being assigned to variables",
+"commas", 
+"curly brackets",
+"quotes", 
+"parenthesis"];
+const fiveChoice = ["A very useful tool used during development and debugging for printing content to the debugger is:",
+"JavaScript", 
+"terminal/bash", 
+"for loops", 
+"console.log"];
 const correctAnswers = ["alerts", "parenthesis", "all of the above", "quotes", "console.log"];
 
 var startButton = document.querySelector("#start-button");
@@ -25,15 +40,24 @@ var secondsLeft = timer.textContent;
 startButton.addEventListener("click", function(event) {
     startTimer();
     startScreen.setAttribute("hidden", "true");
-    startScreen.setAttribute("display", "");
+    // startScreen.setAttribute("display", "");
     questionText.removeAttribute("hidden", "true");
-    questionText.textContent = questions[0];
-    button1.textContent = oneChoice[0];
-    button2.textContent = oneChoice[1];
-    button3.textContent = oneChoice[2];
-    button4.textContent = oneChoice[3];
+    questionText.textContent = oneChoice[0];
+    button1.textContent = oneChoice[1];
+    button2.textContent = oneChoice[2];
+    button3.textContent = oneChoice[3];
+    button4.textContent = oneChoice[4];
     answerButtons.removeAttribute("hidden", "true");
+    button1.addEventListener("click", handleAnswerClick)
+    button2.addEventListener("click", handleAnswerClick)
+    button3.addEventListener("click", handleAnswerClick)
+    button3.addEventListener("click", handleAnswerClick)
+    button4.addEventListener("click", handleAnswerClick)
 });
+
+function handleAnswerClick (event) {
+    console.log("click worked ", event.target);
+}
 
 function startTimer() {
     // Sets timer
