cda457c0044ab5bfb1663540e490e1459909aa8e
Jacob Scobie <jscobie@focus-solutions.net>
2022-10-09 18:32:50 -0400

    sunday night progress, very close to working

--- assets/script.js
+++ assets/script.js
@@ -26,9 +26,8 @@ const questions = [
         answer: "console.log"
     }
 ]
-var questionIndex = 0;
-var secondsLeft = timer.textContent;
 
+// variables for handling html elements
 var startButton = document.querySelector("#start-button");
 var startScreen = document.querySelector("#start-screen")
 var timer = document.querySelector("#countdown");
@@ -38,41 +37,90 @@ var button1 = document.querySelector("#choice1");
 var button2 = document.querySelector("#choice2");
 var button3 = document.querySelector("#choice3");
 var button4 = document.querySelector("#choice4");
+var result = document.querySelector("#feedback");
+var resultScreen = document.querySelector("#end-screen");
+var playerScore = document.querySelector("#score");
 
+// variables for timing
+var questionIndex = 0;
+var secondsLeft = timer.textContent;
 
-
-startButton.addEventListener("click", function(event) {
-    startTimer();
+// Starts the quiz: Hides the start section, loads the question/answer, calls time to start
+function playGame() {
     startScreen.setAttribute("hidden", "true");
-    // startScreen.setAttribute("display", "");
     questionText.removeAttribute("hidden", "true");
-    questionText.textContent = questions[questionIndex].question;
-    button1.textContent = questions[questionIndex].choices[0];
-    button2.textContent = questions[questionIndex].choices[1];
-    button3.textContent = questions[questionIndex].choices[2];
-    button4.textContent = questions[questionIndex].choices[3];
     answerButtons.removeAttribute("hidden", "true");
-    button1.addEventListener("click", handleAnswerClick)
-    button2.addEventListener("click", handleAnswerClick)
-    button3.addEventListener("click", handleAnswerClick)
-    button3.addEventListener("click", handleAnswerClick)
-    button4.addEventListener("click", handleAnswerClick)
-
-});
-
-function handleAnswerClick (event) {
-    console.log(event.target.textContent);
-    questionIndex++;
-    console.log(questionIndex);
+    startTimer();
+    pullQuestion();
 }
 
+// Sets timer and runs it
 function startTimer() {
-    // Sets timer
     timerS = setInterval(function() {
         secondsLeft--;
         timer.textContent = secondsLeft;
         if (secondsLeft === 0) {
             clearInterval(timerS);
+            endGame();
         }
-        }, 100);
-      }
+    }, 1000);
+};
+
+// Gets question from the questions array and applys them to the quiz section (question and 4 answer buttons)
+function pullQuestion() {
+    var openQuestion = questions[questionIndex];
+    questionText.textContent = questions[questionIndex].question;
+    
+    button1.textContent = questions[questionIndex].choices[0];
+    button2.textContent = questions[questionIndex].choices[1];
+    button3.textContent = questions[questionIndex].choices[2];
+    button4.textContent = questions[questionIndex].choices[3];
+}
+
+function answer() {
+    result.removeAttribute("hidden", "true");
+    if (this.textContent !== questions[questionIndex].answer) {
+        secondsLeft -= 15;
+        result.textContent = "Wrong!!! Time lost.";
+        if (secondsLeft < 0) {
+            secondsLeft = 0;
+        }
+        timer.textContent = secondsLeft;
+    } else {
+        result.textContent = "Correct, Good Job!"
+    }
+
+    // only show the result of last click for a brief period (1.5 seconds = 1500)
+    setTimeout(function() {
+        result.setAttribute("hidden", "true");
+    }, 1500);
+
+    // increase index to go to next question
+    questionIndex++;
+
+    // if we are done with questions end, otherwise get another question set
+    if (questionIndex === questions.length) {
+        endGame();
+    } else {
+        pullQuestion();
+    }
+}
+
+function endGame() {
+    clearInterval(timerS);
+    questionText.setAttribute("hidden", "true");
+    answerButtons.setAttribute("hidden", "true");
+    resultScreen.removeAttribute("hidden", "true");
+    result.setAttribute("hidden", "true");
+    playerScore.textContent = secondsLeft;
+}
+
+// button to start playing the game
+startButton.addEventListener("click", playGame);
+
+// button listeners for the answer buttons
+button1.addEventListener("click", answer)
+button2.addEventListener("click", answer)
+button3.addEventListener("click", answer)
+button4.addEventListener("click", answer)
+    
